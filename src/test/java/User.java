ab50569009801fa064580d592a4df6634c846937
DariaZinchenko <dariadaria0593@gmail.com>
2021-03-10 22:39:09 +0300

    Use lombok

--- src/test/java/User.java
+++ src/test/java/User.java
@@ -1,23 +1,10 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class User {
     private final String login;
     private final String password;
     private final String status;
-
-    public User(String login, String password, String status) {
-        this.login = login;
-        this.password = password;
-        this.status = status;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getStatus() {
-        return status;
-    }
 }
